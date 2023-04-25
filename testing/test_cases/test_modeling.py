28979e328bcb6abb13da2262b5a864d1934b12d9
Wenshuai Hou <wenhoujx@gmail.com>
2023-04-24 20:36:08 -0400

    fix.

--- testing/test_cases/test_modeling.py
+++ testing/test_cases/test_modeling.py
@@ -10,24 +10,52 @@ def _load_yaml(path: str) -> Dict:
 
 class TestModeling:
     def test_simple(self):
-        conn = load_from_schema(_load_yaml("testing/test_schemas/one_node_one_table.yml"))
+        conn = load_from_schema(
+            _load_yaml("testing/test_schemas/one_node_one_table.yml")
+        )
         res = conn.execute("match (p:Person)  return p").get_as_arrow(chunk_size=200)
         assert res.num_rows == 10, "10 rows expected"
         res = conn.execute(
             "match (p: Person) where p.name = 'John Smith' return p.age as age"
         ).get_as_arrow(chunk_size=200)
-        assert res.to_pydict()['age'] == [24]
+        assert res.to_pydict()["age"] == [24]
 
-    def test_one_node_two_tables(self): 
-        conn = load_from_schema(_load_yaml("testing/test_schemas/one_node_two_tables.yml"))
+    def test_one_node_two_tables(self):
+        conn = load_from_schema(
+            _load_yaml("testing/test_schemas/one_node_two_tables.yml")
+        )
         res = conn.execute("match (p:Person)  return p").get_as_arrow(chunk_size=200)
         assert res.num_rows == 10, "10 rows expected"
-        res = conn.execute("match (p:Person {name: 'Mary Anderson'}) return p.gender as gender").get_as_df()
-        assert res.iloc[0,0] == 'Male'
-        res = conn.execute("match (p:Person {name: 'John Smith'}) return p.gender as gender").get_as_df()
-        assert res.iloc[0,0] == 'Female'
+        res = conn.execute(
+            "match (p:Person {name: 'Mary Anderson'}) return p.gender as gender"
+        ).get_as_df()
+        assert res.iloc[0, 0] == "Male"
+        res = conn.execute(
+            "match (p:Person {name: 'John Smith'}) return p.gender as gender"
+        ).get_as_df()
+        assert res.iloc[0, 0] == "Female"
 
-    def test_two_nodes_one_table(self): 
-        conn = load_from_schema(_load_yaml("testing/test_schemas/two_nodes_one_table.yml"))
-        res = conn.execute("match (p:Person {name: 'Mary Anderson' }) -[:LIVES_IN]-> (s:State)  return s.name").get_as_df()
-        assert(res.iloc[0,0] == 'Texas')
+    def test_two_nodes_one_table(self):
+        conn = load_from_schema(
+            _load_yaml("testing/test_schemas/two_nodes_one_table.yml")
+        )
+        res = conn.execute(
+            "match (p:Person {name: 'Mary Anderson' }) -[:LIVES_IN]-> (s:State)  return s.name"
+        ).get_as_df()
+        assert res.iloc[0, 0] == "Texas"
+
+    def test_two_nodes_two_tables(self):
+        conn = load_from_schema(
+            _load_yaml("testing/test_schemas/two_nodes_two_tables.yml")
+        )
+        res = conn.execute(
+            "match (p:Person {name: 'Mary Anderson' }) -[:LIVES_IN]-> (s:State)  return s.short_name"
+        ).get_as_df()
+        assert res.iloc[0, 0] == "TX"
+
+    def test_self_referring(self):
+        conn = load_from_schema(_load_yaml("testing/test_schemas/self_reference.yml"))
+        res = conn.execute(
+            "match (e:Employee {name: 'Jane Doe' }) -[:REPORTS_TO]-> (m:Employee)  return m.name"
+        ).get_as_df()
+        assert res.iloc[0, 0] == "John Smith"
--- testing/test_cases/test_play.py
+++ testing/test_cases/test_play.py
@@ -35,13 +35,14 @@ def test_int64():
 def test_load_data_with_fewer_columns():
     db = kuzu.Database(f"./testdbs/test-{_random_id()}")
     conn = kuzu.Connection(db)
-    # this fails b/c the csv file has fewer columns than the schema. 
+    # this fails b/c the csv file has fewer columns than the schema.
     conn.execute(
         "CREATE NODE TABLE Person(id STRING, first_name STRING, last_name STRING,  age INT64, state STRING, PRIMARY KEY (id))"
     )
     conn.execute("COPY Person FROM './data/persons.csv' (header=true)")
 
-def test_load_data_by_column_name(): 
+
+def test_load_data_by_column_name():
     db = kuzu.Database(f"./testdbs/test-{_random_id()}")
     conn = kuzu.Connection(db)
     # this fails b/c kuzu loading by index position instead of column name.
@@ -49,5 +50,23 @@ def test_load_data_by_column_name():
         "CREATE NODE TABLE Person(id STRING,  age INT64, name STRING,  state STRING, PRIMARY KEY (id))"
     )
     conn.execute("COPY Person FROM './data/persons.csv' (header=true)")
+    res = conn.execute("match (p: Person) return p.name").get_as_df()
+    print(res)
+
+
+def test_load_two_tables():
+    db = kuzu.Database(f"./testdbs/test-{_random_id()}")
+    conn = kuzu.Connection(db)
+    # this fails b/c kuzu loading by index position instead of column name.
+    conn.execute(
+        "CREATE NODE TABLE Person(id STRING,  name STRING, age INT64, PRIMARY KEY (id))"
+    )
+    conn.execute(
+        "create node table State(name STRING, short_name STRING, PRIMARY KEY (name))"
+    )
+    conn.execute("create rel table LIVES_IN(from Person to State)")
+    conn.execute("COPY  Person FROM './data/persons.csv' (header=true)")
+    conn.execute("COPY State FROM './data/states.csv' (header=true)")
+    conn.execute("COPY LIVES_IN from './data/lives_in.csv' (header=true)")
     res = conn.execute("match (p: Person) return p.name").get_as_df()
     print(res)
